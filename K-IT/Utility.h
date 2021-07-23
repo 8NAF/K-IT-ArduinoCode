a89b18a29e66e9fb8283ad4a5625f5a51475d5e2
Mai Anh Khoa <maikhoa000@gmail.com>
2021-07-23 14:55:45 +0700

    fomat code hàm printStageChange

--- K-IT/Utility.h
+++ K-IT/Utility.h
@@ -12,7 +12,7 @@ void delayAndPrint(int seconds) {
 void printStageChange(const char* label, byte previousState, byte currentState) {
 	
 	const char * previous = (previousState == HIGH ? "HIGH" : "LOW");
-	const char* current = (currentState == HIGH ? "HIGH" : "LOW");
+	const char * current  = (currentState  == HIGH ? "HIGH" : "LOW");
 	
 	Serial.printf("%s: %s -> %s\n", label, previous, current);
 }
